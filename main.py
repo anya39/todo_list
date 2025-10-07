520f186f3dbfe20fa11920907434e87d610b4673
Anya Schenck <anya.schenck@gmail.com>
2025-10-07 14:08:41 -0400

    changed formatting from .pack to .grid

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 #setup main window
 root = tk.Tk()
 root.title("To-Do List")
-root.geometry("600x700")
+root.geometry("600x800")
 root.configure(bg = "#0A2947")
 
 #function to get text from task entry
@@ -36,32 +36,32 @@ def complete_task():
 
 #top label
 label = tk.Label(root, text="To-Do List",font=("Times New Roman", 30), fg="#FFFFFF", bg="#0A2947")
-label.pack(pady=10)
+label.grid(row=0, column=0, columnspan=4, pady=20)
 
 #task entry field
 task_entry = tk.Text(root, width=40, height= 3, font=("Times New Roman", 25), fg="#224C6E", bg="#DEECFC")
-task_entry.pack(pady=20)
+task_entry.grid(row=1, column=2, padx=35)
 
 #buttons --------------------------------------------------
 #add task button
 add_button = tk.Button(root, text="Add Task", font=("Times New Roman", 20), command= get_task )
-add_button.pack()
+add_button.grid(row=2, columnspan=4, pady=10, sticky="w", padx=35)
 
 #delete task button
 delete_button = tk.Button(root, text="Delete Task", font=("Times New Roman", 20), command = delete_task )
-delete_button.pack(pady=10)
+delete_button.grid(row=3, columnspan=4, sticky="w", padx=35)
 
 #delete all tasks button
 delete_all_button = tk.Button(root, text="Clear All", font=("Times New Roman", 20), command = delete_all)
-delete_all_button.pack(pady=8)
+delete_all_button.grid(row=2, columnspan=4, sticky="e", padx=35)
 
 #complete task button
 complete_button = tk.Button(root, text="Complete Task", font=("Times New Roman", 20), command = complete_task)
-complete_button.pack(pady=10)
+complete_button.grid(row=3, columnspan=4, sticky="e", padx=35)
 
 #listbox to display tasks
 task_listbox = tk.Listbox(root, width=50, height=15, font=("Times New Roman", 22), fg="#000000", bg="#E5DCFF", selectbackground="#7FEBAE")
-task_listbox.pack(pady=20)
+task_listbox.grid(row=4, column=0, columnspan=4, pady=40)
 
 #goes at end of program
 root.mainloop()
